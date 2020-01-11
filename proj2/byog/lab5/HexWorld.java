6c56e88b905f8b3ce6a7d82ad736ccd83828e9c7
lensherrggg <gr98522@gmail.com>
2020-01-11 23:09:01 +0800

    proj2 preparation, addHexagon added

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,52 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    public static void addHexagon(TETile[][] world, Position pos, int size, TETile t) {
+        if (size < 2) {
+            throw new IllegalArgumentException("Hexagon must be at least size 2.");
+        }
+        for (int i = 0; i < 2 * size; i++) {
+            addRow(world, pos, i, size, t);
+        }
+    }
+
+    private static int offsetOfLine(int lineNum, int size) {
+        if (lineNum < size) {
+            return size - 1 - lineNum;
+        } else {
+            return lineNum - size;
+        }
+    }
+
+    private static int widthOfLine(int lineNum, int size) {
+        if (lineNum < size) {
+            return size + 2 * lineNum;
+        } else {
+            return 5 * size - 2 - 2 * lineNum;
+        }
+    }
+
+    private static void addRow(TETile[][] world, Position pos, int lineNum, int size, TETile t) {
+        int offset = offsetOfLine(lineNum, size);
+        for (int j = 0; j < widthOfLine(lineNum, size); j++) {
+            world[pos.x + offset + j + 1][pos.y + lineNum] = t;
+        }
+    }
+
+    public static void main (String[] args) {
+        int WIDTH = 60;
+        int HEIGHT = 30;
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                world[i][j] = Tileset.NOTHING;
+            }
+        }
+        Position pos = new Position(3, 0);
+        addHexagon(world, pos, 5, Tileset.FLOWER);
+        ter.renderFrame(world);
+
+    }
 }
---
+++ proj2/byog/lab5/Position.java
@@ -0,0 +1,10 @@
+package byog.lab5;
+
+public class Position {
+    int x;
+    int y;
+    public Position(int xPos, int yPos) {
+        x = xPos;
+        y = yPos;
+    }
+}
