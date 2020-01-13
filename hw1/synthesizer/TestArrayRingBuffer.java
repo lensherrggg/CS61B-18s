3027062a54bf766be6efd2e8c245a30c166b2459
lensherrggg <gr98522@gmail.com>
2020-01-13 12:10:33 +0800

    hw1 task1-4 finished task5 remains

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -10,6 +10,28 @@ public class TestArrayRingBuffer {
     @Test
     public void someTest() {
         //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        BoundedQueue<Integer> arb = new ArrayRingBuffer<>(5);
+        assertEquals(arb.capacity(), 5);
+        assertTrue(arb.isEmpty());
+        arb.enqueue(1);
+        arb.enqueue(2);
+        assertFalse(arb.isEmpty());
+        assertEquals(arb.peek(), Integer.valueOf(1));
+
+        Integer ret = arb.dequeue();
+        assertEquals(ret, Integer.valueOf(1));
+        assertEquals(arb.peek(), Integer.valueOf(2));
+        ret = arb.dequeue();
+        assertEquals(ret, Integer.valueOf(2));
+
+        for (int i = 5; i >= 1; i--) {
+            arb.enqueue(i);
+        }
+        assertTrue(arb.isFull());
+        assertEquals(arb.peek(), Integer.valueOf(5));
+        ret = arb.dequeue();
+        assertFalse(arb.isFull());
+        assertEquals(ret, Integer.valueOf(5));
     }
 
     /** Calls tests for ArrayRingBuffer. */
