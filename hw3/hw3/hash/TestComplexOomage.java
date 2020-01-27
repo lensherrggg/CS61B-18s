4537dc169927fbda4bd55d9cad1368f28dcb5e32
lensherrggg <gr98522@gmail.com>
2020-01-27 19:57:35 +0800

    hw3 finished

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -1,5 +1,6 @@
 package hw3.hash;
 
+import edu.princeton.cs.algs4.In;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -36,15 +37,29 @@ public class TestComplexOomage {
     /* TODO: Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
+
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
+        // According to hashCode() in ComplexOomage.java and Hint.java,
+        // if the hashCode exceeds 256e3, it will overflow to 0.
+        // Use add(i) rather than add(some_constant) will let built-in
+        // hashCode() to pass.
+
         // Your code here.
+        for (int i = 0; i < 10; i++) {
+            ArrayList<Integer> params = new ArrayList<>();
+            params.add(i);
+            for (int j = 0; j < 4; j++) {
+                params.add(1);
+            }
+            ComplexOomage com = new ComplexOomage(params);
+            deadlyList.add(com);
+        }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
